1779e775b46a29fb1e4ad1205a35ce7f6242af2e
Aleksandr Mikhalkin <tallumglad@yandex.ru>
2021-03-09 18:49:39 +0400

    Thrird stage

--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        System.out.println(i);
-        i = i + 1;
-        System.out.println(i);
-        i = i + 1;
-        System.out.println(i);
-        System.out.println("Hello World!");
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + "!");
     }
 }
